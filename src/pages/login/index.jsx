7073526120609db3eb41538e82f14b6b9f8186d2
zhangailing <1581564405@qq.com>
2019-06-21 20:15:06 +0800

    '完成了表单的登录校验'

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,36 @@
 import React,{Component} from 'react';
 import logo from './img/logo.png'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button} from 'antd';
 import './index.less'
-export default class Login extends Component{
+class Login extends Component{
+    validator=(rule, value, callback)=>{
+        const name=rule.fullField==='username'?'用户名':'密码'
+        if(!value){
+            callback(`${name}不能为空`)
+        } else if(value.length<4){
+            callback(`${name}长度必须大于4`)
+        }else if(value.length>15){
+            callback(`${name}长度必须小于15`)
+        }else if(!/^[a-zA-Z0-9_$]+$/.test(value)){
+            callback(`${name}只能是数字字母下划线和$`)
+        }else {
+            callback()
+        }
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault()
+        const  { validateFields } = this.props.form;
+        validateFields((error, values)=>{
+            if (!error){
+                const {username,password}=values
+                console.log(username,password)
+            }else {
+                console.log('登录表单校验失败：', error);
+            }
+        })
+    }
     render(){
+        const { getFieldDecorator } = this.props.form;
         return(
             <div className='loginWarp'>
                 <header className='loginHeader'>
@@ -17,20 +44,30 @@ export default class Login extends Component{
                     <h2>用户登录</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [{
+                                    // required: true, message: 'Please input your username!'
+                                    validator:this.validator
+                                }],
+                            })(
                                 <Input
                                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                     placeholder="用户名"
                                     className='loginInput'
                                 />
+                            )}
                         </Form.Item>
                         <Form.Item>
-
+                            {getFieldDecorator('password', {
+                                rules: [{  validator:this.validator}],
+                            })(
                                 <Input
                                     prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                     type="password"
                                     placeholder="密码"
                                     className='loginInput'
                                 />
+                            )}
 
                         </Form.Item>
                         <Form.Item>
@@ -46,3 +83,4 @@ export default class Login extends Component{
         )
     }
 }
+export default Form.create()(Login);
